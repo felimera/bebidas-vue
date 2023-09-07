f574bad427ffcd7c1c809d0c8492f7b4a5313ddd
andres.mera <feli041089@gmail.com>
2023-09-07 13:16:15 -0500

    Finalizando el componete que muestra la bebida

--- src/components/Receta.vue
+++ src/components/Receta.vue
@@ -7,8 +7,18 @@ defineProps({
 </script>
 
 <template>
-    <div>
-        <h2>{{ receta.strDrink }}</h2>
+    <div class="border shadow-lg">
+        <div class="overflow-hidden">
+            <img class="hover:scale-105 transition-transform hover:rotate-1" :src="receta.strDrinkThumb"
+                :alt="'Imagen de ' + receta.strDrink">
+        </div>
+
+        <div class="p-5">
+            <h2 class="text-2xl truncate font-extrabold">{{ receta.strDrink }}</h2>
+            <button class="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg">
+                Ver Receta
+            </button>
+        </div>
     </div>
 </template>
 
