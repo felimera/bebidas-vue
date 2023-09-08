a0baedac63f124235fb687928639f45c94c171fa
andres.mera <feli041089@gmail.com>
2023-09-08 08:53:19 -0500

    Agregando recetas a favoritos

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -74,7 +74,8 @@ const formatearIngredientes = () => {
 
                                 <button type="button"
                                     class="w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500"
-                                    @click="favoritos.handleClickFavorito">Agregar a Favoritos</button>
+                                    @click="favoritos.handleClickFavorito">Agregar a Favoritos
+                                </button>
                             </div>
                         </DialogPanel>
                     </TransitionChild>
--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,11 +1,17 @@
+import { ref } from "vue";
 import { defineStore } from "pinia";
+import { useBebidasStore } from "./bebidas";
 
 export const useFavoritoStore = defineStore("favoritos", () => {
+  const bebidas = useBebidasStore();
+  const favoritos = ref([]);
+
   const handleClickFavorito = () => {
-    console.log("agregando...");
+    favoritos.value.push(bebidas.receta);
   };
 
   return {
+    favoritos,
     handleClickFavorito,
   };
 });
